0882e4ac59fc1672ebcff5a2e34c92f2d8af755b
Thinh1203 <thinhquach1203@gmail.com>
2023-02-03 20:08:31 +0700

    first commit

--- mylibrary/public/admin/xoanguoidung.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include '../../connect.php';
-    use App\Models\user;
-
-    $id = $_GET['id'];
-    $sql = user::where('id' , $id)->delete();
-    header('Location: lietkesanpham.php?layout=danhsachsp');
-?>
--- mylibrary/public/admin/xoasanpham.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include '../../connect.php';
-    use App\Models\Product;
-
-    $id = $_GET['id'];
-    $sql = Product::where('id' , $id)->delete();
-    header('Location: lietkesanpham.php?layout=danhsachsp');
-?>
